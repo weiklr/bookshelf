2670c1ed1f0c71cf727561c074b6b13c628e0097
weiklr <wayne@pop-os.localdomain>
2021-01-28 15:03:22 +0800

    documented what i learn from this exercise

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -7,6 +7,15 @@ import {useAuth} from './context/auth-context'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
+/**
+ * *LWK NOTES
+ * 1. learnt how to create custom hooks and react context to shared states and functions across components.
+ *    for e.g. useAuth, AuthProvider, and useClient
+ *
+ * 2. learnt that React.callback can be used to provide memoizeed function in a API
+ *    so that users can safely use it in useEffects as a dependency in other parts of the code
+ */
+
 function App() {
   const {user} = useAuth()
   console.log('user is ', user)
